7aec4161ffd78e3fe90ec1627f6defe0f6aeda69
Ashish Chaudhary <sk8896107@gmail.com>
2026-08-30 15:36:51 +0530

    Refactor teacher routes to enable student search, dashboard, and fees retrieval

--- src/routes/teacher.route.js
+++ src/routes/teacher.route.js
@@ -1,13 +1,13 @@
-// import express from "express";
-// import teacherController from "../controllers/teacher.controller.js";
-// const router = express.Router();
+import express from "express";
+import teacherController from "../controllers/teacher.controller.js";
+const router = express.Router();
 
-// // router.get("/dashboard", teacherController.teacherDetails);
-// // router.get("/search", teacherController.teacherDetails);
-// // router.get("/classes", teacherController.teacherSubjects);
-// // router.get("/attendance", teacherController.teacherSubjects);
-// // router.get("/timetable", teacherController.teacherSubjects);
-// // router.get("/students", teacherController.teacherSubjects);
-// // router.get("/fees", teacherController.teacherSubjects);
+router.get("/search", protect, teacherController.searchStudent);
+router.get("/dashboard", protect, teacherController.teacherDashboard);
+router.get("/students", protect, teacherController.getStudents);
+router.get("/student/:studentId", protect, teacherController.getStudentDetails);
+router.get("/subjects", protect, teacherController.getTeacherSubject);
+router.get("/subject/:subjectId", protect, teacherController.getSubjectDetails);
+router.get("/fees", protect, teacherController.getStudentFess);
 
-// export default router;
+export default router;
